120bfe10420ffaf43732a1a0406c9abd2c66d9f1
David Munro <david.munro@nokia.com>
2019-12-19 16:17:04 -0500

    Added global style to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,25 @@
-import { configure } from '@storybook/react';
+/** @jsx jsx */
+
+import { configure, addDecorator } from '@storybook/react';
+import { Global, css, jsx } from '@emotion/core'
+
+export const bodyStyles = css`
+  font-family: 'Roboto', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
+`
+
+addDecorator(story => (
+  <div>
+    <Global
+      styles={css`
+          body {
+            ${bodyStyles}
+          }
+        `}
+    />
+    {story()}
+  </div>
+));
+
 
 const req = require.context('../src', true, /\.stories.(js|jsx)$/);
 
