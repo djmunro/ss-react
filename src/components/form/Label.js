29eda23e705f9866203e4686a61189e50a19c2ad
David Munro <david.munro@nokia.com>
2019-12-20 11:52:59 -0500

    Update label css

--- src/components/form/Label.js
+++ src/components/form/Label.js
@@ -1,5 +1,10 @@
 import styled from '@emotion/styled';
 
-const Label = styled.label``;
+const Label = styled.label`
+  display: block;
+  font-size: 1.6rem;
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+`;
 
 export default Label;
